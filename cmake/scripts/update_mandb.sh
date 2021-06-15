0bdb08d995d6ad77a09f5080b399fe4b9d97152e
An7ar35 <eadavison@protonmail.com>
2021-06-15 20:26:37 +0200

    [CMAKE] Update of script for updating the man database so that it only gets run when the build type is explicitly 'Release'.

--- cmake/scripts/update_mandb.sh
+++ cmake/scripts/update_mandb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    printf "Not running as root\n"
-    exit
+printf "Build type: '%s'\n" "$2"
+
+if [[ "$2" == "Release" ]]; then
+  if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+      printf "Not running as root\n"
+      exit
+  else
+      printf "Updating mandb...\n"
+      mandb --quiet
+  fi
 else
-    printf "Updating man db...\n"
-    mandb --quiet
+  printf "Skipping mandb update.\n"
 fi
