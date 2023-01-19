cce0a9f8bd8567ff636d48a15bfbe74ccb1d244a
An7ar35 <eadavison@protonmail.com>
2023-01-19 19:11:22 +0100

    [Docker] Added new "ubuntu" build and changed GID to the host audio group's;

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,11 +2,12 @@
 
 declare -A MAP=(
   ["arch"]="build-arch"
+  ["ubuntu"]="build-ubuntu"
 )
 
 if [[ -n "${MAP[$1]}" ]]; then
   printf "Launching docker build: %s\n" "${MAP[$1]}"
-  HOST_UID="$(id -u)" HOST_GID="$(id -g)" HOST_UNAME="$(whoami)" docker-compose -f docker-compose.yml run --rm "${MAP[$1]}"
+  HOST_UID="$(id -u)" HOST_GID="$(id -g)" HOST_UNAME="$(whoami)" AUDIO_GID="$(getent group audio | cut -d: -f3)" docker-compose -f docker-compose.yml run --rm "${MAP[$1]}"
   docker rmi ctune-"${MAP[$1]}":latest
 else
   printf "Invalid argument. Builds available:\n"
